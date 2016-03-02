45f37478c65718d280ea481b1bf0e8c6e891b6d4
acbozkurt <acbozkurt@windowslive.com>
2016-03-02 15:48:21 +0200

    RUYİDEM-BİTTİ

--- app/config/config.php
+++ app/config/config.php
@@ -5,13 +5,13 @@
  * istenilen her yerinden ulaşabiliriz.Bu kısma aşağıdaki gibi bir tanımalam yeterli gelecektir.
  */
 //sitenin url bilgisi
-define("SITE_URL", "http://localhost/SProject");
+define("SITE_URL", "http://localhost/ruyidem");
 
 //site front end 
-define("SITE_FRONT_ASSETS", "http://localhost/SProject/app/views/Template_FrontEnd/assets");
-define("SITE_FRONT_ASSETSCSS", "http://localhost/SProject/app/views/Template_FrontEnd/assets/css");
-define("SITE_FRONT_ASSETSJS", "http://localhost/SProject/app/views/Template_FrontEnd/assets/js");
-define("SITE_FRONT_ASSETSIMG", "http://localhost/SProject/app/views/Template_FrontEnd/assets/img");
-define("SITE_FRONT_ASSETSIMAGES", "http://localhost/SProject/app/views/Template_FrontEnd/assets/images");
+define("SITE_FRONT_ASSETS", "http://localhost/ruyidem/app/views/Template_FrontEnd/assets");
+define("SITE_FRONT_ASSETSCSS", "http://localhost/ruyidem/app/views/Template_FrontEnd/assets/css");
+define("SITE_FRONT_ASSETSJS", "http://localhost/ruyidem/app/views/Template_FrontEnd/assets/js");
+define("SITE_FRONT_ASSETSIMG", "http://localhost/ruyidem/app/views/Template_FrontEnd/assets/img");
+define("SITE_FRONT_ASSETSIMAGES", "http://localhost/ruyidem/app/views/Template_FrontEnd/assets/images");
 
 ?>
