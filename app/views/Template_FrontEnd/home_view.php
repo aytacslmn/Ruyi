df11e44389b78b896c728744682f88c7ec500042
acbozkurt <acbozkurt@windowslive.com>
2016-03-03 21:09:43 +0200

    RUYİDEM-BİTTİ-responsive

--- app/views/Template_FrontEnd/home_view.php
+++ app/views/Template_FrontEnd/home_view.php
@@ -3,60 +3,40 @@
         <ol class="carousel-indicators">
             <li data-target="#main-slider" data-slide-to="0" class="active"></li>
             <li data-target="#main-slider" data-slide-to="1"></li>
-            <li data-target="#main-slider" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
-            <div class="item active" style="background-image: url(<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/bg5.png)">
+            <div class="item active" style="background-image: url(<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/bg6.png)">
                 <div class="container">
                     <div class="row slide-margin">
-                        <div class="col-sm-6">
-                            <div class="carousel-content"style="margin-top:-40px">
-                                <h1 class="animation animated-item-1 kod1" >Genç ve Dinamik Ekibimizle</h1>
-                                <h2 class="animation animated-item-2 ekip" >Buradayız</h2>
+                        <div class="col-sm-12 col-md-3">
+                            
+                        </div>
+                        <div class="col-sm-12 col-md-9" style="margin-top:-250px" >
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1 kod" style=" color:#2C2929" ><b>Genç ve Dinamik Ekibimizle</b></h1>
+                                <h2 class="animation animated-item-2 ekip" style=" color:#2C2929"><b>Buradayız</b></h2>
                             </div>
                         </div>
-                        <div class="col-sm-6 animation animated-item-4">
-                            <div class="slider-img">
-                                <img src="<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/img5.png" class="img-responsive">
-                            </div>
-                        </div>
+                       
                     </div>
                 </div>
             </div><!--/.item-->
             <div class="item" style="background-image: url(<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/bg2.png)">
                 <div class="container">
                     <div class="row slide-margin">
-                        <div class="col-sm-6 animation animated-item-4">
-                            <div class="slider-img hidden-xs">
-                                <img src="<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/img6.png" class="img-responsive">
-                            </div>
-                        </div> 
-                        <div class="col-sm-6">
+                        <div class="col-sm-12 col-md-5">
+                            
+                        </div>
+                        <div class="col-sm-6 col-md-7" style="margin-top:310px">
                             <div class="carousel-content" style="margin-top:-40px">
-                                <h1 class="animation animated-item-1 kod1" >Prestijli Projelerle</h1>
-                                <h2 class="animation animated-item-2 ekip1">Yükseliyoruz</h2>
+                                <h1 class="animation animated-item-1 kod1" style=" color:fff" ><b>Prestijli Projelerle</b></h1>
+                                <h2 class="animation animated-item-2 ekip1" style=" color:fff"><b>Yükseliyoruz</b></h2>
                             </div>
                         </div>
                     </div>
                 </div>
             </div><!--/.item-->
-            <div class="item" style="background-image: url(<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/bg3.png)">
-                <div class="container">
-                    <div class="row slide-margin">
-                        <div class="col-sm-6">
-                            <div class="carousel-content " style="margin-top:-40px">
-                                <h1 class="animation animated-item-1 kod1" >Etik ve Kaliteli Yazılımda</h1>
-                                <h2 class="animation animated-item-2 ekip2">Bize Güvenin</h2>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 animation animated-item-4">
-                            <div class="slider-img">
-                                <img src="<?php echo SITE_FRONT_ASSETSIMAGES; ?>/slider/img3.png" class="img-responsive" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div><!--/.item-->
+            
         </div><!--/.carousel-inner-->
     </div><!--/.carousel-->
     <a class="prev " href="#main-slider" data-slide="prev">
